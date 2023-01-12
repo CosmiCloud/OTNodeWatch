09b01b229e4182f325bf5af54591060c7530984c
CosmiCloud <42195091+CosmiCloud@users.noreply.github.com>
2023-01-12 12:14:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv')
+require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TOKEN;
 const bot = new TelegramBot(token, {polling: true});
