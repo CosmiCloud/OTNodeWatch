1ae76c54485ca2c22449c8955320d75898a4ec2b
CosmiCloud <tybranha@gmail.com>
2023-01-19 20:26:54 -0500

    commit

--- pubCount.js
+++ pubCount.js
@@ -14,7 +14,9 @@ async function pubCount () {
 
   console.log(publishes_last_hour)
 
-  msg = `${publishes_last_hour.stdout} assets have been minted in the last hour.`
+  msg = `${JSON.stringify(
+    publishes_last_hour.stdout
+  )} assets have been minted in the last hour.`
 
   console.log(msg)
   await bot.sendMessage(chatId, msg)
